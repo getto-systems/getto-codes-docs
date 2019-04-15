86550165fb0ac22b9456398664cc04484b220484
shun <shun@getto.systems>
2019-04-15 16:44:08 +0000

    add: docs

---
+++ bin/stat.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+getto_codes_docs_stat_main(){
+  local target_dir
+  local target
+  local data_root
+  local limit_files
+  local limit_lines
+  local limit_hierarchy
+  local all_files
+  local all_lines
+  local min_lines
+  local max_lines
+  local all_hierarchy
+  local min_hierarchy
+  local max_hierarchy
+  local file
+  local count
+  local all
+  local min
+  local max
+
+  target_dir=$1; shift
+  target=$1; shift
+
+  data_root=$APP_ROOT/data
+
+  limit_files=20
+  limit_lines=80
+  limit_hierarchy=4
+
+  all_files=$(find docs -type f -not -name index.md)
+
+  all_lines=0
+  min_lines=
+  max_lines=0
+
+  all_hierarchy=0
+  min_hierarchy=
+  max_hierarchy=0
+
+  for file in $all_files; do
+    count=$(cat $file | wc -l)
+    all=$all_lines
+    min=$min_lines
+    max=$max_lines
+    getto_codes_docs_stat_sum
+    all_lines=$all
+    min_lines=$min
+    max_lines=$max
+
+    count=$(echo $file | sed 's|[^/]||g' | wc -c)
+    all=$all_hierarchy
+    min=$min_hierarchy
+    max=$max_hierarchy
+    getto_codes_docs_stat_sum
+    all_hierarchy=$all
+    min_hierarchy=$min
+    max_hierarchy=$max
+  done
+
+  all_files=$(echo "$all_files" | wc -l)
+
+  getto_codes_docs_stat_dump_badge "files" $limit_files $all_files
+  getto_codes_docs_stat_dump_badge "lines" $limit_lines $all_lines $min_lines $max_lines
+  getto_codes_docs_stat_dump_badge "hierarchy" $limit_hierarchy $all_hierarchy $min_hierarchy $max_hierarchy
+}
+getto_codes_docs_stat_sum(){
+  if [ -z "$min" ]; then
+    min=$count
+  else
+    if [ $count -lt $min ]; then
+      min=$count
+    fi
+  fi
+
+  if [ $count -gt $max ]; then
+    max=$count
+  fi
+
+  all=$(($all + $count))
+}
+getto_codes_docs_stat_dump_badge(){
+  local badge
+  local limit
+  local all_count
+  local min_count
+  local max_count
+  local avg_count
+  local message
+  local color
+
+  badge=$1; shift
+  limit=$1; shift
+  all_count=$1; shift
+  min_count=$1; shift
+  max_count=$1; shift
+
+  if [ $badge = "files" ]; then
+    message=$all_files
+    color=success
+  else
+    avg_count=$(($all_count * 10 / $all_files))
+    limit=$(($limit * 10))
+
+    if [ $avg_count -gt $limit ]; then
+      color=critical
+    else
+      color=success
+    fi
+
+    message=$avg_count
+  fi
+
+  curl -s "https://img.shields.io/static/v1.svg?style=for-the-badge&label=${badge}&message=${message}&color=${color}" > ${data_root}/${badge}.svg
+  echo "${badge}: ${color}"
+}
+
+getto_codes_docs_stat_main "$@"
---
+++ docs/elm/index.md
@@ -0,0 +1,10 @@
+# GETTO Codes : Elm
+
+## どう使うのか？
+
+- [Page を作成する](use/page.md)
+
+
+## どう拡張するのか？
+
+- [モジュールの配置方法](extend/module.md)
